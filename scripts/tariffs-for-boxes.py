dc3c4b42d502768766d905ad8f5dfbe013b1e7fb
Ilya19951110 <ilyasachenko@gmail.com>
2025-05-28 21:58:50 +0300

    uploaded script tariffs boxes

---
+++ scripts/tariffs-for-boxes.py
@@ -0,0 +1,56 @@
+from gspread_dataframe import set_with_dataframe
+import json
+import requests
+import os
+import pandas as pd
+import gspread
+
+
+def tariffs_for_boxes():
+
+    gs = gspread.service_account(filename='key.json')
+    url = "https://common-api.wildberries.ru/api/v1/tariffs/box"
+
+    spreadsheet = gs.open('Ассортиментная матрица. Полная')
+    sheets = spreadsheet.worksheet('API(Тарифы коробов)')
+
+    headers = {
+        "Authorization": os.getenv('Rachel')
+    }
+
+    params = {
+        "date": '2025-01-01'
+    }
+
+    res = requests.get(url, headers=headers, params=params)
+    result = res.json()
+
+    box = pd.DataFrame(result['response']['data']['warehouseList'])
+
+    box['dtNextBox'] = result['response']['data']['dtNextBox']
+    box['dtTillMax'] = result['response']['data']['dtTillMax']
+
+    box = box.rename(columns={
+        'boxDeliveryAndStorageExpr': 'Доставка_и_хранение',
+        'boxDeliveryBase': 'Доставка_базовая',
+        'boxDeliveryLiter': 'Доставка_за_литр',
+        'boxStorageBase': 'Хранение_база_день',
+        'boxStorageLiter': 'Хранение_за_литр_день',
+        'warehouseName': 'Склад'
+    })
+
+    target_col = [col for col in box.columns if col not in box.columns[-3:]]
+
+    box[target_col] = box[target_col].replace('-', 0)
+
+    box[target_col] = box[target_col].replace(',', '.', regex=True)
+
+    box[target_col] = box[target_col].apply(
+        pd.to_numeric, errors='coerce').fillna(0)
+
+    set_with_dataframe(
+        sheets, box, include_column_header=False, include_index=True)
+
+
+if __name__ == '__main__':
+    tariffs_for_boxes()
